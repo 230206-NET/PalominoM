366620f4aef9eb7bccc04053c510b1c77794dd53
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2023-02-07 13:29:38 -0800

    Fixed little bug

--- HotandCold/Program.cs
+++ HotandCold/Program.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 
-namespace HotOrCold {
+namespace HotOrCold
+{
 
-    public class HOC {
+    public class HOC
+    {
         public static void Main(string[] args)
         {
             var ran = new Random(21);
             int target = ran.Next(21);
-            Console.WriteLine("Target" + target);
-            Console.WriteLine("GUESS:");
+            Console.WriteLine("Target: " + target);
+            Console.WriteLine("GUESS: ");
             string input = Console.ReadLine();
             int num = int.Parse(input);
-            
-            while (num != target) {
-                
-                input = Console.ReadLine();
-                num = int.Parse(input);
-                
+
+            while (num != target)
+            {
                 if (num > target)
                 {
                     Console.WriteLine("Too High. Guess Again:");
@@ -25,11 +24,13 @@ namespace HotOrCold {
                 {
                     Console.WriteLine("Too Low. Guess Again:");
                 }
+                input = Console.ReadLine();
+                num = int.Parse(input);
             }
             Console.WriteLine("YAY! You did it :)");
-            
-            
+
+
         }
     }
-    
+
 }

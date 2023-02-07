be50fe67af120e591db78d99a8a7ccd68635d8db
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2023-02-07 13:04:22 -0800

    Commit Basic Game

---
+++ HotandCold/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HotOrCold {
+
+    public class HOC {
+        public static void Main(string[] args)
+        {
+            var ran = new Random();
+            int target = ran.Next(21);
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+            if (num == target) {
+                Console.WriteLine("YAY");
+            }
+            else if (num > target)
+            {
+                Console.WriteLine("Too High");
+            }
+            else
+            {
+                Console.WriteLine("Too Low");
+            }
+        }
+    }
+    
+}

a63e5d7342b7d342e650b7014214c2d5ef45290d
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2023-02-08 15:23:15 -0800

    Update to Budget App

--- BudgetApp/Program.cs
+++ BudgetApp/Program.cs
@@ -8,39 +8,75 @@ Display the all expenses and the remaining amount
 using System;
 using System.Collections.Generic;
 
-//Title of app
-Console.WriteLine("BUDGET APP");
+namespace BudgetApp
+{
+    public class BudgetCalculator
+    {
+        // decimal budgetMoney;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("BUDGET APP");
+            //create list of items
+            List<Item> budget = new List<Item>();
+            //User input
+            //add try and catch if user inputs another thing 
+            Console.WriteLine("Input initial budget (MUST BE INT):");
+            int budgetInitial = int.Parse(Console.ReadLine()!);
 
-//User input
-int budgetInitial = int.Parse(Console.WriteLine("Input initial budget")!);
-Console.WriteLine("Enter expense: Amount and Description");
-string input = Console.ReadLine();
-string temp;
-int amount = 0;
-for (int i = 0; i < input.Length; i++) {
-    if (Char.IsDigit(input[i])) {
-        temp += input[i];
-        input[i] = " ";
-    }    
-}
+            Console.WriteLine("Enter expense: Amount and Description");
+            string input = Console.ReadLine()!;
+            string temp = string.Empty;
+            int amount = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsDigit(input[i]))
+                {
+                    temp += input[i];
+                }
+            }
+            amount = int.Parse(temp); //must be decimal 
+            string description = string.Concat(input.Where(char.IsLetter));
+            Console.WriteLine(description + " " + amount); //testing purpose
 
-Console.WriteLine(temp);
-amount = int.Parse(temp);
+
+            //diplay list
+            foreach (Item item in budget)
+            {
+                Console.WriteLine(item.Description());
+            }
+
+        }
+
+        //Method to calculate remaining amount
+        private decimal CalculateRemain(List<Item> budgetList, int budget)
+        {
+            decimal total = 0;
+            foreach (Item item in budgetList)
+            {
+                total += item.getCost();
+            }
+            return budget - total;
+        }
+    }
 
 
-//Method to calculate remaining amount
-private decimal CalculateRemain() {
-    return 0; 
-}
+    public record Item
+    {
+        private string description { get; set; }
+        private decimal cost { get; set; }
 
-public record Item { 
-    private string description { get; set; }
-    private int cost { get; set; }
+        public string Description()
+        {
+            return "Cost: " + cost + "  Description: " + description;
+        }
+        public decimal getCost()
+        {
+            return cost;
+        }
 
-    public string Description() {
-        return "Cost: " + cost + "  Description: " + description;
     }
 
 }
+
 
 

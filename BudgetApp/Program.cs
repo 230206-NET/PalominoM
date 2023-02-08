366ff1638d3b14d4aadeb02fbf204e8a4933c86e
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2023-02-08 14:53:14 -0800

    BA update

---
+++ BudgetApp/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Create a console application that will help you track your expenses, and thus survive in this economy! Your app should be able to do the following
+
+Prompt the user to enter the initial budget
+Prompt the user to enter expenses with amount and description
+Display the all expenses and the remaining amount
+ */
+using System;
+using System.Collections.Generic;
+
+//Title of app
+Console.WriteLine("BUDGET APP");
+
+//User input
+int budgetInitial = int.Parse(Console.WriteLine("Input initial budget")!);
+Console.WriteLine("Enter expense: Amount and Description");
+string input = Console.ReadLine();
+string temp;
+int amount = 0;
+for (int i = 0; i < input.Length; i++) {
+    if (Char.IsDigit(input[i])) {
+        temp += input[i];
+        input[i] = " ";
+    }    
+}
+
+Console.WriteLine(temp);
+amount = int.Parse(temp);
+
+
+//Method to calculate remaining amount
+private decimal CalculateRemain() {
+    return 0; 
+}
+
+public record Item { 
+    private string description { get; set; }
+    private int cost { get; set; }
+
+    public string Description() {
+        return "Cost: " + cost + "  Description: " + description;
+    }
+
+}
+
+

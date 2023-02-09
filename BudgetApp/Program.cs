ad00302a758a055a2b439edf1bd3b04605287fa2
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2023-02-09 07:58:20 -0800

    Some small changes to Budget program

--- BudgetApp/Program.cs
+++ BudgetApp/Program.cs
@@ -8,10 +8,18 @@ Display the all expenses and the remaining amount
 using System;
 using System.Collections.Generic;
 
+/*
+    - Need to check if input is number 
+    - check if number is not negative
+    - check if expense input is still valid 
+    - check if user wants to continue input is actually valid
+ 
+ */
 namespace BudgetApp
 {
     public class BudgetCalculator
     {
+
         // decimal budgetMoney;
         public static void Main(string[] args)
         {
@@ -20,24 +28,10 @@ namespace BudgetApp
             List<Item> budget = new List<Item>();
             //User input
             //add try and catch if user inputs another thing 
-            Console.WriteLine("Input initial budget (MUST BE INT):");
+            Console.WriteLine("Input initial budget (MUST BE NUMBER):");
             int budgetInitial = int.Parse(Console.ReadLine()!);
 
-            Console.WriteLine("Enter expense: Amount and Description");
-            string input = Console.ReadLine()!;
-            string temp = string.Empty;
-            int amount = 0;
-            for (int i = 0; i < input.Length; i++)
-            {
-                if (Char.IsDigit(input[i]))
-                {
-                    temp += input[i];
-                }
-            }
-            amount = int.Parse(temp); //must be decimal 
-            string description = string.Concat(input.Where(char.IsLetter));
-            Console.WriteLine(description + " " + amount); //testing purpose
-
+            promptUser(budget);
 
             //diplay list
             foreach (Item item in budget)
@@ -45,10 +39,21 @@ namespace BudgetApp
                 Console.WriteLine(item.Description());
             }
 
+            decimal remaining = calculateRemaining(budget, budgetInitial);
+            if (remaining < 0)
+            {
+                Console.WriteLine("You are way over budget! Money left:" + remaining);
+            }
+            else
+            {
+                Console.WriteLine("You have enough money. Money left:" + remaining);
+            }
+
+
         }
 
         //Method to calculate remaining amount
-        private decimal CalculateRemain(List<Item> budgetList, int budget)
+        private static decimal calculateRemaining(List<Item> budgetList, decimal budget)
         {
             decimal total = 0;
             foreach (Item item in budgetList)
@@ -57,6 +62,50 @@ namespace BudgetApp
             }
             return budget - total;
         }
+
+        private static void promptUser(List<Item> list)
+        {
+            bool end = false;
+            while (end == false)
+            {
+                Console.WriteLine("Enter expense: Amount and Description");
+                string input = Console.ReadLine()!;
+                addToList(list, input);
+                Console.WriteLine("Would you like to add another expense? (Y/N)");
+                string ans = Console.ReadLine()!;
+                if (ans == "Y" || ans == "y")
+                {
+                    end = false;
+                }
+                else if (ans == "N" || ans == "n")
+                {
+                    end = true;
+                }
+            }
+
+        }
+
+        private static List<Item> addToList(List<Item> list, string input)
+        {
+            string temp = string.Empty;
+            int amountTemp = 0; decimal amount = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsDigit(input[i]))
+                {
+                    temp += input[i];
+                }
+            }
+            if (temp.Length > 0)
+            {
+                amountTemp = int.Parse(temp); //must be decimal 
+                amount = amountTemp;
+            }
+
+            string description = string.Concat(input.Where(char.IsLetter));
+            list.Add(new Item(description, amount));
+            return list;
+        }
     }
 
 
@@ -64,6 +113,12 @@ namespace BudgetApp
     {
         private string description { get; set; }
         private decimal cost { get; set; }
+
+        public Item(string des, decimal cost)
+        {
+            this.description = des;
+            this.cost = cost;
+        }
 
         public string Description()
         {
